245c75c98fc49a2d679b050c9b3a7b2704ed4f5f
nadiaputrirahmawati <nadiaputrirahman@gmail.com>
2024-12-30 11:49:30 +0700

    migration

---
+++ app/Models/Movie.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected $fillable = [
+        'title',
+        'synopsis',
+        'poster',
+        'year',
+        'available',
+        'genre_id',
+    ];
+}
